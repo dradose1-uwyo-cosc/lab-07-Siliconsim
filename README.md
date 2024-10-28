04533c2e94423807544396c9fd94f57e7c5a9d2b
Jake Huggins <119003692+Siliconsim@users.noreply.github.com>
2024-10-28 22:08:53 +0000

    Finished lab and changed README

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# [Name]
-## [Assignment #]
-## [Submission date: MM/DD/YY]
-## Worked with/sources 
-* example
-* example
-* example
-* example
+# Jake Huggins
+## Lab 07
+## 10/28/2024
+## Worked with/sources
+* https://www.w3schools.com/python/ref_string_split.asp
+* https://www.w3schools.com/python/ref_string_replace.asp
+
 ## Project Quirks/ Things that don't work
-* example
-* example
-* example
+* None
--- cosc1010-lab07.py
+++ cosc1010-lab07.py
@@ -1,13 +1,9 @@
-# Your Name Here
+# jake Huggins
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
-
+# 10/28
+# Lab 14
+# Lab Section: 14
+# Sources, people worked with, help given to: https://www.w3schools.com/python/ref_string_split.asp, https://www.w3schools.com/python/ref_string_replace.asp
 
 # Prompt the user for an upper bound 
 # Write a while loop that gives the factorial of that upper bound
@@ -16,8 +12,16 @@
     # To do so you can use the methods `.isdigit()` or `.isnumeric()`
     # If a user did not enter a number output a statement saying so
 # You will continue to prompt the user until a proper integer value is entered
-
+while (True):
+    upper_bound = input("Please input an upper bound: ")
+    if upper_bound.isnumeric():
+        break
+    print("Input is not valid, please try again.")
 factorial = 1
+
+upper_bound = int(upper_bound)
+for i in range(1, upper_bound + 1):
+    factorial *= i
 
 print(f"The result of the factorial based on the given bound is {factorial}")
 
@@ -37,7 +41,28 @@ print("*"*75)
 # All this together means you will have an intensive while loop that includes multiple if statements, likely with some nesting 
 # The sum should start at 0 
 
+user_int = 0
+is_negative = False
+while (True):
+    temp = input("Please enter a whole number: ")
+    if (temp.find("-") == 0):
+        is_negative = True
+    
+    user_int = temp.lstrip().replace("-", "")
+    if (user_int.isdigit()):
+        break
+    print("Input is not valid, please try again.")
+user_int = int(user_int)
 num_sum = 0 
+
+print(is_negative)
+
+if is_negative:
+    for i in range (1, user_int + 1):
+        num_sum -= i
+else:
+    for i in range(1, user_int + 1):
+        num_sum += i
 
 print(f"Your final sum is {num_sum}")
 
@@ -59,4 +84,29 @@ print("*"*75)
 # Print the result of the equation
 # Again, loop through prompting the user for input until `exit` in any casing is input 
 
-        
+operators = ["+", "-", "*", "%", "/"]
+
+user_input = input("Please enter an expression. E.g., 2 + 2: ")
+operator = ""
+for char in user_input:
+    if char in operators:
+        operator = char
+    elif (char == " "):
+        user_input = user_input.replace(char, "")
+
+string_digits = user_input.split(operator)
+digits = [None] * len(string_digits)
+
+for i in range(0, len(string_digits)):
+    digits[i] = int(string_digits[i])
+
+if (operator == "+"):
+    print(f"{digits[0]} + {digits[1]} = {digits[0] + digits[1]}")
+elif (operator == "-"):
+    print(f"{digits[0]} - {digits[1]} = {digits[0] - digits[1]}")
+elif (operator == "*"):
+    print(f"{digits[0]} * {digits[1]} = {digits[0] * digits[1]}")
+elif (operator == "/"):
+    print(f"{digits[0]} / {digits[1]} = {digits[0] / digits[1]}")
+elif (operator == "%"):
+    print(f"{digits[0]} % {digits[1]} = {digits[0] % digits[1]}")
