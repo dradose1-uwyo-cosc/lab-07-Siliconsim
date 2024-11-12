9fd5f6444f524a76c4680aa3472d900ae8203331
Jake Huggins <119003692+Siliconsim@users.noreply.github.com>
2024-11-12 06:22:28 +0000

    asdasd

--- huggins-cosc1010-hw04.py
+++ huggins-cosc1010-hw04.py
@@ -11,13 +11,13 @@ cleaned_contents = ""
 output_string = ""
 
 def output_char(pair):
+    print(pair)
     if (len(pair) <= 1):
         return
     global output_string
     output_char = mappings[pair[0]]
     
     times_to_output = int(pair[1])
-    
     for i in range(times_to_output):
         output_string += output_char
 
@@ -31,9 +31,8 @@ except FileNotFoundError:
 cleaned_contents = contents.replace("\n", "")
 cleaned_contents = cleaned_contents.split("\t")
 
+print(len(cleaned_contents))
 # Split each key value pair in the list into separate individual lists 
-for kvp in cleaned_contents:
-    #key_value_pairs.append(kvp.split(":"))
-    output_char(kvp.split(":"))
-
-print(output_string)
+for i in range(0, len(cleaned_contents)):
+    output_char(cleaned_contents[i].split(":"))
+print("\n" + output_string)
